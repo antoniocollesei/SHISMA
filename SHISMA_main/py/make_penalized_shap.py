368b65a7de03ada82266749a28a3e254fb6196ac
Antonio Collesei <antonio.collesei@gmail.com>
2026-04-20 16:07:01 +0200

    Cleaner implementation of BORF, and code fixes

--- SHISMA_main/py/make_penalized_shap.py
+++ SHISMA_main/py/make_penalized_shap.py
@@ -34,47 +34,55 @@ args = parser.parse_args()
 
 # Load data
 data = pd.read_csv(args.time_series, index_col=0).dropna(axis=0)
-data.index = data.index.str.split('_').map(lambda x: (x[0], x[-1]))
-data.index = pd.MultiIndex.from_tuples(data.index, names=["gene", "celltype"])
-data_avg = data.groupby(level=["gene", "celltype"]).mean().reset_index()
-data_avg["gene_celltype"] = data_avg["gene"] + "_" + data_avg["celltype"]
-data_avg = data_avg.set_index("gene_celltype").drop(columns=["gene", "celltype"])
+index_dims = data.index.str.split('_', n=2, expand=True)
+index_dims.names = ["gene", "patient", "celltype"]
+data.index = index_dims
 
-ppi_genes = pd.read_csv(args.ppi, sep="\t")
-ppi_genes = pd.concat([ppi_genes["gene1"], ppi_genes["gene2"]]).unique()
-data = data_avg.loc[data_avg.index.str.split('_').str[0].isin(ppi_genes)]
+# Early PPI Filtering
+ppi_df = pd.read_csv(args.ppi, sep="\t")
+ppi_genes = np.unique(ppi_df[["gene1", "gene2"]].values.astype(str))
+data = data[data.index.get_level_values("gene").isin(ppi_genes)]
+data_avg = data.groupby(level=["gene", "celltype"]).mean()
 
-# Encoding
-genes = np.array([name.split("_")[0] for name in list(data.index)])
-cells = np.array([name.split("_")[1] for name in list(data.index)])
+# --- 2. Encoding & Matrix Prep ---
+genes_vec = data_avg.index.get_level_values("gene")
+cells_vec = data_avg.index.get_level_values("celltype")
+
 enc_genes, enc_cells = LabelEncoder(), LabelEncoder()
-enc_genes.fit(genes)
-enc_cells.fit(cells)
-X, y_genes, y_cells = data.values[:, np.newaxis, :], enc_genes.transform(genes), enc_cells.transform(cells)
+y_genes = enc_genes.fit_transform(genes_vec)
+y_cells = enc_cells.fit_transform(cells_vec)
+X = data_avg.values[:, np.newaxis, :]
 
 # BORF setup
-builder = BorfBuilder(n_jobs=-2, configs=CUSTOM_CONFIG_A3_NO_DILATION_WINDOW_SIZE_2_3_4,
-    pipeline_objects=[(ReshapeTo2D, dict(keep_unraveled_index=True)), (ZeroColumnsRemover, dict(axis=0)), (ToScipySparse, dict())])
+builder = BorfBuilder(
+    n_jobs=args.num_cores, 
+    configs=CUSTOM_CONFIG_A3_NO_DILATION_WINDOW_SIZE_2_3_4,
+    pipeline_objects=[
+        (ReshapeTo2D, dict(keep_unraveled_index=True)), 
+        (ZeroColumnsRemover, dict(axis=0)), 
+        (ToScipySparse, dict())
+    ]
+)
 borf = builder.build(X)
 X_transformed = borf.fit_transform(X)
 
+cell_type_indexes = np.where(enc_cells.inverse_transform(y_cells) == args.cell_type)[0]
+X_eval = X_transformed.toarray()[cell_type_indexes]
+
+# Train Decision tree classifier and compute SHAP values
 clf = DecisionTreeClassifier()
 clf.fit(X_transformed, y_genes)
 cell_type_indexes = np.where(enc_cells.inverse_transform(y_cells) == args.cell_type)[0]
 
-num_permutations, num_cores = args.num_permutations, args.num_cores
-
-shap_explainer = fasttreeshap.TreeExplainer(clf, algorithm='auto', n_jobs=num_cores)
-shap_values = shap_explainer(X_transformed.toarray()[cell_type_indexes], check_additivity=False).values
+shap_explainer = fasttreeshap.TreeExplainer(clf, algorithm='auto', n_jobs=args.num_cores)
+shap_values = shap_explainer(X_eval, check_additivity=False).values
 average = np.mean(abs(shap_values), axis=0)
 average_df = pd.DataFrame(average, columns=enc_genes.classes_.astype(str))
-
-X_transformed_arr = X_transformed.toarray()[cell_type_indexes]
 
 penalization_method = args.penalization_method  # 'permutation' or 'maxT' or 'Westfall-Young'
 
 # Permutation testing with MHT correction
-if penalization_method == 'bonferroni':
+if args.penalization_method == 'bonferroni':
   
   penalization_threshold = args.penalization_threshold  # Original significance level
   num_features = average.shape[1]  # Number of features (genes)
@@ -83,17 +91,17 @@ if penalization_method == 'bonferroni':
   def compute_permutation(i):
     y_genes_perm = np.random.permutation(y_genes)
     model = DecisionTreeClassifier().fit(X_transformed, y_genes_perm)
-    shap_explainer = fasttreeshap.TreeExplainer(model, algorithm='auto', n_jobs=num_cores)
-    shap_values_perm = shap_explainer(X_transformed_arr, check_additivity=False).values
+    shap_explainer = fasttreeshap.TreeExplainer(model, algorithm='auto', n_jobs=args.num_cores)
+    shap_values_perm = shap_explainer(X_eval, check_additivity=False).values
     return np.einsum('ijk->jk', np.abs(shap_values_perm)) / shap_values_perm.shape[0]
 
-  average_perm_list = np.array(Parallel(n_jobs=num_cores)(delayed(compute_permutation)(i) for i in range(num_permutations)))
+  average_perm_list = np.array(Parallel(n_jobs=args.num_cores)(delayed(compute_permutation)(i) for i in range(args.num_permutations)))
   p_values = pd.DataFrame(np.mean(average_perm_list >= average, axis=0))
   penalization_mask = (p_values <= bonferroni_threshold).astype(int)
   shap_values_pval_penalized = abs(average * penalization_mask)
   shap_values_pval_penalized.columns = enc_genes.classes_.astype(str)
 
-elif penalization_method == 'fdr':
+elif args.penalization_method == 'fdr':
   
   penalization_threshold = args.penalization_threshold  # Original significance level
   num_features = average.shape[1]  # Number of features (genes)
@@ -101,11 +109,11 @@ elif penalization_method == 'fdr':
   def compute_permutation(i):
     y_genes_perm = np.random.permutation(y_genes)
     model = DecisionTreeClassifier().fit(X_transformed, y_genes_perm)
-    shap_explainer = fasttreeshap.TreeExplainer(model, algorithm='auto', n_jobs=num_cores)
-    shap_values_perm = shap_explainer(X_transformed_arr, check_additivity=False).values
+    shap_explainer = fasttreeshap.TreeExplainer(model, algorithm='auto', n_jobs=args.num_cores)
+    shap_values_perm = shap_explainer(X_eval, check_additivity=False).values
     return np.einsum('ijk->jk', np.abs(shap_values_perm)) / shap_values_perm.shape[0]
 
-  average_perm_list = np.array(Parallel(n_jobs=num_cores)(delayed(compute_permutation)(i) for i in range(num_permutations)))
+  average_perm_list = np.array(Parallel(n_jobs=args.num_cores)(delayed(compute_permutation)(i) for i in range(args.num_permutations)))
   p_values = pd.DataFrame(np.mean(average_perm_list >= average, axis=0))
 
   p_values_corrected = multipletests(p_values.values.flatten(), alpha=penalization_threshold, method='fdr_bh')[1]
--- SHISMA_main/wrapper.sh
+++ SHISMA_main/wrapper.sh
@@ -7,31 +7,35 @@ default_num_cores=1
 default_penalization_method="fdr"
 default_penalization_threshold=0.05
 
+run() {
+    "$@" > /dev/null 2>&1
+}
+
 while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --data) time_series_file="$2"; shift 2 ;;
-    --ppi) ppi_file="$2"; shift 2 ;;
-    --ct) cell_type="$2"; shift 2 ;;
-    --out) output_name="$2"; shift 2 ;;
-    --mht) penalization_method="$2"; shift 2 ;;
-    --alpha) penalization_threshold="$2"; shift 2 ;;
-    --nperm) num_permutations="$2"; shift 2 ;;
-    --cores) num_cores="$2"; shift 2 ;;
-    *) echo "Unknown parameter passed: $1"; exit 1 ;;
-  esac
+    case $1 in
+        --data) time_series_file="$2"; shift 2 ;;
+        --ppi) ppi_file="$2"; shift 2 ;;
+        --ct) cell_type="$2"; shift 2 ;;
+        --out) output_name="$2"; shift 2 ;;
+        --mht) penalization_method="$2"; shift 2 ;;
+        --alpha) penalization_threshold="$2"; shift 2 ;;
+        --nperm) num_permutations="$2"; shift 2 ;;
+        --cores) num_cores="$2"; shift 2 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
 done
 
-# Apply default values if not provided
 num_permutations="${num_permutations:-$default_num_permutations}"
 num_cores="${num_cores:-$default_num_cores}"
 penalization_method="${penalization_method:-$default_penalization_method}"
 penalization_threshold="${penalization_threshold:-$default_penalization_threshold}"
 
 if [[ -z "$time_series_file" || -z "$ppi_file" || -z "$cell_type" || -z "$output_name" ]]; then
-  echo "Usage: $0 --data <time_series_file> --ppi <ppi_file> --ct <cell_type> --out <output_name> [--mht <penalization_method>] [--alpha <penalization_threshold>] [--nperm <num_permutations>] [--cores <num_cores>]"
-  exit 1
+    echo "Usage: $0 --data <time_series_file> --ppi <ppi_file> --ct <cell_type> --out <output_name> [--mht <penalization_method>] [--alpha <penalization_threshold>] [--nperm <num_permutations>] [--cores <num_cores>]"
+    exit 1
 fi
 
+echo "Analysis of $time_series_file started. PPI being used is $ppi_file"
 start_time=$(date +%s)
 
 output_folder=analysis_"$output_name"
@@ -39,147 +43,133 @@ data=$PWD/"$output_folder"/shapelet_data
 intermediate=$PWD/"$output_folder"/shapelet_intermediate
 results=$PWD/"$output_folder"/shapelet_results
 
-mkdir -p $output_folder
-mkdir -p $data
-mkdir -p $intermediate
-mkdir -p $results
+run mkdir -p "$output_folder"
+run mkdir -p "$data"
+run mkdir -p "$intermediate"
+run mkdir -p "$results"
 
-# Set the correct Python path from your virtual environment
-python py/make_penalized_shap.py \
-    --time_series "$time_series_file" \
-    --ppi "$ppi_file" \
-    --cell_type "$cell_type" \
-    --output_folder "$output_folder" \
-    --penalization_method "$penalization_method" \
-    --penalization_threshold "$penalization_threshold" \
-    --num_permutations "$num_permutations" \
-    --num_cores "$num_cores"
+echo "Generating gene-shape scores..."
 
-################################################################################
-#   Prepare data.
-################################################################################
+run python py/make_penalized_shap.py \
+        --time_series "$time_series_file" \
+        --ppi "$ppi_file" \
+        --cell_type "$cell_type" \
+        --output_folder "$output_folder" \
+        --penalization_method "$penalization_method" \
+        --penalization_threshold "$penalization_threshold" \
+        --num_permutations "$num_permutations" \
+        --num_cores "$num_cores"
+
+echo "Completed."
 
 network=$(basename "$ppi_file" | sed 's/\.[^.]*$//')
-mkdir -p $intermediate/"$network"
+run mkdir -p "$intermediate/$network"
 
-# Identify available score files
-score_files=($(ls $data/scores_*.tsv 2>/dev/null | xargs -n 1 basename | sed 's/.tsv//'))
+score_files=($(ls "$data"/scores_*.tsv 2>/dev/null | xargs -n 1 basename | sed 's/.tsv//'))
 
 for score_file in "${score_files[@]}"
 do
-    mkdir -p $intermediate/"$network"_"$score_file"
+        run mkdir -p "$intermediate/${network}_${score_file}"
 done
 
-################################################################################
-#   Construct similarity matrices.
-################################################################################
+echo "Constructing similarity matrices for network diffusion..."
 
-echo "Construct similarity matrices..."
+run python py/src-hierarchical-hotnet/construct_similarity_matrix.py \
+        -i   "$PWD/$output_folder/${network}_edge_list.tsv" \
+        -o   "$intermediate/$network/similarity_matrix.h5" \
+        -bof "$intermediate/$network/beta.txt"
 
-python py/src-hierarchical-hotnet/construct_similarity_matrix.py \
-    -i   $PWD/"$output_folder"/"$network"_edge_list.tsv \
-    -o   $intermediate/"$network"/similarity_matrix.h5 \
-    -bof $intermediate/"$network"/beta.txt
-
-################################################################################
-#   Permute data.
-################################################################################
+echo "Completed."
 
 echo "Permuting networks..."
 
-cp $PWD/"$output_folder"/"$network"_index_gene.tsv $intermediate/"$network"/index_gene_0.tsv
-cp $PWD/"$output_folder"/"$network"_edge_list.tsv $intermediate/"$network"/edge_list_0.tsv
+run cp "$PWD/$output_folder/${network}_index_gene.tsv" "$intermediate/$network/index_gene_0.tsv"
+run cp "$PWD/$output_folder/${network}_edge_list.tsv" "$intermediate/$network/edge_list_0.tsv"
 
-parallel -u -j $num_cores --bar \
-    python py/src-hierarchical-hotnet/permute_network.py \
-        -i $intermediate/"$network"/edge_list_0.tsv \
-        -s {} \
-        -c \
-        -o $intermediate/"$network"/edge_list_{}.tsv \
-    ::: $(seq 1 4)
+run parallel -u -j "$num_cores" \
+        python py/src-hierarchical-hotnet/permute_network.py \
+                -i "$intermediate/$network/edge_list_0.tsv" \
+                -s {} \
+                -c \
+                -o "$intermediate/$network/edge_list_{}.tsv" \
+        ::: $(seq 1 4)
 
-parallel -u -j $num_cores --bar \
-    python py/src-hierarchical-hotnet/permute_network.py \
-        -i $intermediate/"$network"/edge_list_0.tsv \
-        -s {} \
-        -o $intermediate/"$network"/edge_list_{}.tsv \
-    ::: $(seq 5 8)
+run parallel -u -j "$num_cores" \
+        python py/src-hierarchical-hotnet/permute_network.py \
+                -i "$intermediate/$network/edge_list_0.tsv" \
+                -s {} \
+                -o "$intermediate/$network/edge_list_{}.tsv" \
+        ::: $(seq 5 8)
+
+echo "Completed."
 
 echo "Permuting scores..."
 
 for score_file in "${score_files[@]}"
 do
-    cp $data/"$score_file".tsv $intermediate/"$network"_"$score_file"/scores_0.tsv
+        run cp "$data/$score_file.tsv" "$intermediate/${network}_${score_file}/scores_0.tsv"
 
-    python py/src-hierarchical-hotnet/find_permutation_bins.py \
-        -gsf $intermediate/"$network"_"$score_file"/scores_0.tsv \
-        -igf $PWD/"$output_folder"/"$network"_index_gene.tsv \
-        -elf $PWD/"$output_folder"/"$network"_edge_list.tsv \
-        -ms  1000 \
-        -o   $intermediate/"$network"_"$score_file"/score_bins.tsv
+        run python py/src-hierarchical-hotnet/find_permutation_bins.py \
+                -gsf "$intermediate/${network}_${score_file}/scores_0.tsv" \
+                -igf "$PWD/$output_folder/${network}_index_gene.tsv" \
+                -elf "$PWD/$output_folder/${network}_edge_list.tsv" \
+                -ms  1000 \
+                -o   "$intermediate/${network}_${score_file}/score_bins.tsv"
 
-    parallel -u -j $num_cores --bar \
-        python py/src-hierarchical-hotnet/permute_scores.py \
-            -i  $intermediate/"$network"_"$score_file"/scores_0.tsv \
-            -bf $intermediate/"$network"_"$score_file"/score_bins.tsv \
-            -s  {} \
-            -o  $intermediate/"$network"_"$score_file"/scores_{}.tsv \
-        ::: $(seq $num_permutations)
+        run parallel -u -j "$num_cores" \
+                python py/src-hierarchical-hotnet/permute_scores.py \
+                        -i  "$intermediate/${network}_${score_file}/scores_0.tsv" \
+                        -bf "$intermediate/${network}_${score_file}/score_bins.tsv" \
+                        -s  {} \
+                        -o  "$intermediate/${network}_${score_file}/scores_{}.tsv" \
+                ::: $(seq "$num_permutations")
 done
 
-################################################################################
-#   Construct hierarchies.
-################################################################################
+echo "Completed."
 
 echo "Constructing hierarchies..."
 
 for score_file in "${score_files[@]}"
 do
-    parallel -u -j $num_cores --bar \
-        python py/src-hierarchical-hotnet/construct_hierarchy.py \
-            -smf  $intermediate/"$network"/similarity_matrix.h5 \
-            -igf  $PWD/"$output_folder"/"$network"_index_gene.tsv \
-            -gsf  $intermediate/"$network"_"$score_file"/scores_{}.tsv \
-            -helf $intermediate/"$network"_"$score_file"/hierarchy_edge_list_{}.tsv \
-            -higf $intermediate/"$network"_"$score_file"/hierarchy_index_gene_{}.tsv \
-        ::: $(seq 0 $num_permutations)
+        run parallel -u -j "$num_cores" \
+                python py/src-hierarchical-hotnet/construct_hierarchy.py \
+                        -smf  "$intermediate/$network/similarity_matrix.h5" \
+                        -igf  "$PWD/$output_folder/${network}_index_gene.tsv" \
+                        -gsf  "$intermediate/${network}_${score_file}/scores_{}.tsv" \
+                        -helf "$intermediate/${network}_${score_file}/hierarchy_edge_list_{}.tsv" \
+                        -higf "$intermediate/${network}_${score_file}/hierarchy_index_gene_{}.tsv" \
+                ::: $(seq 0 "$num_permutations")
 done
-
-################################################################################
-#   Process hierarchies.
-################################################################################
 
 echo "Processing hierarchies..."
 
 for score_file in "${score_files[@]}"
 do
-    target_file="$intermediate/${network}_${score_file}/hierarchy_edge_list_0.tsv"
+        target_file="$intermediate/${network}_${score_file}/hierarchy_edge_list_0.tsv"
 
-    if [[ -f "$target_file" ]]; then
-        
-        python py/src-hierarchical-hotnet/process_hierarchies.py \
-            -oelf "$intermediate/${network}_${score_file}/hierarchy_edge_list_0.tsv" \
-            -oigf "$intermediate/${network}_${score_file}/hierarchy_index_gene_0.tsv" \
-            -pelf $(for i in $(seq $num_permutations); do echo "$intermediate/${network}_${score_file}/hierarchy_edge_list_${i}.tsv"; done) \
-            -pigf $(for i in $(seq $num_permutations); do echo "$intermediate/${network}_${score_file}/hierarchy_index_gene_${i}.tsv"; done) \
-            -lsb  1 \
-            -cf   "$results/clusters_${network}_${score_file}.tsv" \
-            -pl   "$network" "$score_file" \
-            -pf   "$results/sizes_${network}_${score_file}.pdf" \
-            -nc   "$num_cores"
-    else
-        echo "Skipping $score_file because hierarchy not being evaluated for this shape."
-    fi
+        if [[ -f "$target_file" ]]; then
+                run python py/src-hierarchical-hotnet/process_hierarchies.py \
+                        -oelf "$intermediate/${network}_${score_file}/hierarchy_edge_list_0.tsv" \
+                        -oigf "$intermediate/${network}_${score_file}/hierarchy_index_gene_0.tsv" \
+                        -pelf $(for i in $(seq "$num_permutations"); do echo "$intermediate/${network}_${score_file}/hierarchy_edge_list_${i}.tsv"; done) \
+                        -pigf $(for i in $(seq "$num_permutations"); do echo "$intermediate/${network}_${score_file}/hierarchy_index_gene_${i}.tsv"; done) \
+                        -lsb  1 \
+                        -cf   "$results/clusters_${network}_${score_file}.tsv" \
+                        -pl   "$network" "$score_file" \
+                        -pf   "$results/sizes_${network}_${score_file}.pdf" \
+                        -nc   "$num_cores"
+        else
+                echo "Skipping $score_file because hierarchy not being evaluated for this shape."
+        fi
 done
 
+echo "Completed."
 
-################################################################################
-#   Parse results.
-################################################################################
+echo "Parsing results..."
 
-python py/parse_hotnet_results.py --time_series "$time_series_file" --cell_type "$cell_type" --results_dir "$results" --output_folder "$output_folder"
+run python py/parse_hotnet_results.py --time_series "$time_series_file" --cell_type "$cell_type" --results_dir "$results" --output_folder "$output_folder"
 
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
 
-echo "Execution completed in $elapsed_time seconds."
+echo "SHISMA's execution completed in $elapsed_time seconds."
